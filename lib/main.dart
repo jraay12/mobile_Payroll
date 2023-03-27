8ba54c4546d71c3886603e5ab43129784732ffe6
John Ray Canete <johnraycaete@gmail.com>
2023-03-27 19:28:49 +0800

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_appdev_integrated/sample.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-
+    theme: ThemeData(primarySwatch: Colors.lightBlue),
+    home: HomePage(),
   ));
 }
 
