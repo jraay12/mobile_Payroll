9452c7a7e66dfba6f8c8ef05f399a9dfc5be2274
MarkRywell <gopro855@gmail.com>
2023-04-03 08:26:27 +0800

    refactor: :recycle: Navigate main into Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_appdev_integrated/sample.dart';
+import 'package:mobile_appdev_integrated/views/auth/login_page.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(primarySwatch: Colors.lightBlue),
-    home: HomePage(),
+    home: LoginPage(),
   ));
 }
 
