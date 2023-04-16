9e513acf52d1776045d61196605ef16f36a20f57
MarkRywell <gopro855@gmail.com>
2023-04-16 17:28:38 +0800

    refactor: :construction: Update login page

--- lib/views/auth/login_page.dart
+++ lib/views/auth/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:mobile_appdev_integrated/models/api.dart';
 import 'dart:convert';
 import '../admin/add_user_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -13,57 +14,95 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final AssetImage background = const AssetImage('assets/images/background1.jpg');
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
   bool _isLoading = false;
 
-  Future<void> _login() async {
-    setState(() {
-      _isLoading = true;
-    });
+  // Future<void> login() async {
+  //   setState(() {
+  //     _isLoading = true;
+  //   });
+  //
+  //   final String email = emailController.text.trim();
+  //   final String password = passwordController.text.trim();
+  //
+  //   if (email.isEmpty || password.isEmpty) {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       const SnackBar(content: Text('Please enter email and password')),
+  //     );
+  //     return;
+  //   }
+  //
+  //   final Uri url = Uri.parse('http://127.0.0.1:8000/api/login');
+  //   final Map<String, String> headers = <String, String>{
+  //     'Content-Type': 'application/json',
+  //   };
+  //   final Map<String, String> body = <String, String>{
+  //     'email': email,
+  //     'password': password,
+  //   };
+  //   final http.Response response = await http.post(url, headers: headers, body: json.encode(body));
+  //
+  //   if (response.statusCode == 200) {
+  //     final Map<String, dynamic> data = json.decode(response.body);
+  //     final String token = data['token'];
+  //
+  //     final SharedPreferences prefs = await SharedPreferences.getInstance();
+  //     await prefs.setString('token', token);
+  //
+  //     Navigator.pushReplacement(
+  //       context,
+  //       MaterialPageRoute<AddUser>(
+  //         builder: (BuildContext context) => const AddUser(),
+  //       ),
+  //     );
+  //   } else {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       const SnackBar(content: Text('Invalid email or password')),
+  //     );
+  //   }
+  //
+  //   setState(() {
+  //     _isLoading = false;
+  //   });
+  // }
 
-    final String email = _emailController.text.trim();
-    final String password = _passwordController.text.trim();
+  showStatus({required Color color, required String text}) {    // Snackbar to show message of API Response
 
-    if (email.isEmpty || password.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Please enter email and password')),
-      );
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text(text),
+            backgroundColor: color,
+            padding: const EdgeInsets.all(15),
+            behavior: SnackBarBehavior.fixed,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            )
+        )
+    );
+  }
+
+  Future login(context) async {
+
+    if(!formKey.currentState!.validate()) {
       return;
     }
 
-    final Uri url = Uri.parse('http://127.0.0.1:8000/api/login');
-    final Map<String, String> headers = <String, String>{
-      'Content-Type': 'application/json',
+    Map credentials ={
+      'email': emailController.text,
+      'password': passwordController.text
     };
-    final Map<String, String> body = <String, String>{
-      'email': email,
-      'password': password,
-    };
-    final http.Response response = await http.post(url, headers: headers, body: json.encode(body));
 
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> data = json.decode(response.body);
-      final String token = data['token'];
+    var response = await Api.instance.login(credentials);
 
-      final SharedPreferences prefs = await SharedPreferences.getInstance();
-      await prefs.setString('token', token);
-
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute<AddUser>(
-          builder: (BuildContext context) => const AddUser(),
-        ),
-      );
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Invalid email or password')),
-      );
+    if(response.status == "fail") {
+      return showStatus(color: Colors.red, text: response.message);
     }
 
-    setState(() {
-      _isLoading = false;
-    });
+    return showStatus(color: Colors.green, text: response.message);
   }
 
   @override
@@ -102,120 +141,98 @@ class _LoginPageState extends State<LoginPage> {
                   color: Colors.blue[900],
                 ),
                 child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Sign in",
-                        style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      const Text(
-                        "Sign in with your Email and Password",
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      SizedBox(
-                        width: 260,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            labelText: "Email",
-                            labelStyle: const TextStyle(color: Colors.white),
-                            prefixIcon: const Icon(
-                              Icons.email,
-                              color: Colors.white,
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
+                  child: Form(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Sign in",
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
                           ),
-                          style: const TextStyle(color: Colors.white),
-                          onChanged: (value) {
-                            // TODO: Store the email entered by the user
-                          },
                         ),
-                      ),
-                      const SizedBox(
-                        height: 12,
-                      ),
-                      SizedBox(
-                        width: 260,
-                        child: TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: "Password",
-                            labelStyle: const TextStyle(color: Colors.white),
-                            prefixIcon: const Icon(
-                              Icons.lock,
-                              color: Colors.white,
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text(
+                          "Sign in with your Email and Password",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
                           ),
-                          style: const TextStyle(color: Colors.white),
-                          onChanged: (value) {
-                            // TODO: Store the password entered by the user
-                          },
                         ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        alignment: Alignment.center,
-                        width: 260,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          color: Colors.blue,
+                        const SizedBox(
+                          height: 20,
                         ),
-                        child: TextButton(
-                          onPressed: () async {
-                            // TODO: Perform login logic
-                            const email = ""; // TODO: Get email entered by the user
-                            const password = ""; // TODO: Get password entered by the user
-                            final response = await http.post(
-                              Uri.parse('http://127.0.0.1:8000/api/login'),
-                              headers: <String, String>{
-                                'Content-Type': 'application/json; charset=UTF-8',
-                              },
-                              body: jsonEncode(<String, String>{
-                                'email': email,
-                                'password': password,
-                              }),
-                            );
-
-                            if (response.statusCode == 200) {
-                              final token = jsonDecode(response.body)['token'];
-                              // TODO: Store the token in shared preferences
-                              Navigator.pushReplacementNamed(context, '/home');
-                            } else {
-                              // TODO: Handle error
-                            }
-                          },
-                          child: const Text(
-                            'Login',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
+                        SizedBox(
+                          width: 260,
+                          child: TextFormField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              labelText: "Email",
+                              labelStyle: const TextStyle(color: Colors.white),
+                              prefixIcon: const Icon(
+                                Icons.email,
+                                color: Colors.white,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        SizedBox(
+                          width: 260,
+                          child: TextFormField(
+                            controller: passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelText: "Password",
+                              labelStyle: const TextStyle(color: Colors.white),
+                              prefixIcon: const Icon(
+                                Icons.lock,
+                                color: Colors.white,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          width: 260,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.blue,
+                          ),
+                          child: TextButton(
+                            onPressed: () async {
+                              login(context);
+                            },
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
