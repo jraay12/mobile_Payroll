2db6627fe5f8d9ac9a532961f4e231f2f7d58b8e
MarkRywell <gopro855@gmail.com>
2023-04-16 17:28:13 +0800

    feat: :sparkles: Add login API

--- lib/models/api.dart
+++ lib/models/api.dart
@@ -1,12 +1,34 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+import 'package:mobile_appdev_integrated/models/api_response.dart';
+
+
 class Api {
 
   Api.privateConstructor();
-
   static final Api instance = Api.privateConstructor();
+
+  var url = "";
 
   Future login(var data) async {
 
+    try {
+      var response = await http.post(Uri.parse(url), body: convert.jsonEncode(data),
+          headers: {"Content-type" : "application/json"});
 
+      var jsonResponse = await convert.jsonDecode(response.body);
+      ApiResponse apiResponse = ApiResponse(
+        status: jsonResponse['status'],
+        message: jsonResponse['message'],
+        data: jsonResponse['data'] ?? {}
+      );
+
+      return apiResponse;
+    }
+    catch (error) {
+      return error;
+    }
 
   }
 
