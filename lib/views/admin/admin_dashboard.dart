c77e8969facb14c409b068287b7d064e8da6633b
MarkRywell <gopro855@gmail.com>
2023-04-24 12:29:30 +0800

    feat: :construction: Update Admin Dashboard

---
+++ lib/views/admin/admin_dashboard.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AdminDashboard extends StatefulWidget {
+
+  const AdminDashboard({Key? key}) : super(key: key);
+
+  @override
+  State<AdminDashboard> createState() => _AdminDashboardState();
+}
+
+class _AdminDashboardState extends State<AdminDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Admin Dashboard"),
+      ),
+    );
+  }
+}
