94f4e5cc433278a2efb7f06ee5aa4c9708153027
MarkRywell <gopro855@gmail.com>
2023-05-20 10:34:16 +0800

    feat: :sparkles: Initialize dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_appdev_integrated/views/auth/login_page.dart';
-import 'package:mobile_appdev_integrated/views/main/dashboard.dart';
-import 'package:mobile_appdev_integrated/views/main/dashboard.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
  
 
-void main() {
+void main() async {
+
+  await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(primarySwatch: Colors.lightBlue),
-    home: Dashboard(),
+    home: LoginPage(),
   ));
 }
 
