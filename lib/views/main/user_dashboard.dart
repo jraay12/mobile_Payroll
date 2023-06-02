2b510ce91fb3b4893453580874e32c518e319e68
MarkRywell <gopro855@gmail.com>
2023-06-02 21:51:24 +0800

    feat: :sparkles: Add logout function in home

--- lib/views/main/user_dashboard.dart
+++ lib/views/main/user_dashboard.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:floating_action_bubble/floating_action_bubble.dart';
+import 'package:mobile_appdev_integrated/models/api.dart';
+import 'package:mobile_appdev_integrated/views/auth/login_page.dart';
 import 'package:mobile_appdev_integrated/views/main/payrolls_dashboard.dart';
 import 'package:mobile_appdev_integrated/views/main/profile.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserDashboard extends StatefulWidget {
   const UserDashboard({Key? key}) : super(key: key);
@@ -14,6 +19,20 @@ class _UserDashboardState extends State<UserDashboard> with SingleTickerProvider
 
   Future getPayroll() async {
 
+  }
+
+  showStatus({required Color color, required String text}) {    // Snackbar to show message of API Response
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text(text),
+            backgroundColor: color,
+            padding: const EdgeInsets.all(15),
+            behavior: SnackBarBehavior.fixed,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            )
+        )
+    );
   }
 
   late Animation<double> _animation;
@@ -31,8 +50,6 @@ class _UserDashboardState extends State<UserDashboard> with SingleTickerProvider
 
 
     super.initState();
-
-
   }
 
 
@@ -40,55 +57,75 @@ class _UserDashboardState extends State<UserDashboard> with SingleTickerProvider
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          actions: [
-            IconButton(onPressed: () async {
-            }, icon: Icon(Icons.logout))
-          ],
-          backgroundColor: Colors.transparent,
-        ),
-        body: SingleChildScrollView(
-          child: FutureBuilder(
-            future: getPayRoll(),
-            builder: (context, snapshot) {
-              if(snapshot.connectionState == ConnectionState.done) {
-                if(snapshot.hasError) {
-                  return RefreshIndicator(
+      appBar: AppBar(
+        elevation: 0,
+        actions: [
+          IconButton(onPressed: () async {
+
+            final pref = await SharedPreferences.getInstance();
+
+            String prefData = pref.getString('user')!;
+
+            var userData = jsonDecode(prefData);
+
+            print(userData['email']);
+
+            var response = await Api.instance.logout({'email' :userData['email']});
+            if(response.status == 'success') {
+              print(response.message);
+              pref.clear();
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => LoginPage()));
+            }
+            else {
+              showStatus(color: Colors.red, text: "Logout Error");
+            }
+          },
+              icon: Icon(Icons.logout))
+        ],
+        backgroundColor: Colors.transparent,
+      ),
+      body: SingleChildScrollView(
+        child: FutureBuilder(
+          future: getPayRoll(),
+          builder: (context, snapshot) {
+            if(snapshot.connectionState == ConnectionState.done) {
+              if(snapshot.hasError) {
+                return RefreshIndicator(
                     onRefresh: () async {
 
                     },
                     child: Text("USER")
-                  );
-                }
+                );
               }
-              return RefreshIndicator(
-                onRefresh: () async {
+            }
+            return RefreshIndicator(
+              onRefresh: () async {
 
-                },
-                child: SingleChildScrollView(
+              },
+              child: SingleChildScrollView(
 
-                ),
-              );
-            },
-          ),
+              ),
+            );
+          },
         ),
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionBubble(
         items: <Bubble>[
           Bubble(
-            title: "Profile",
-            titleStyle: TextStyle(
-              fontSize: 12
-            ),
-            icon: Icons.person,
-            iconColor: Colors.white,
-            bubbleColor: Colors.blue,
-            onPress: () {
-              Navigator.pushReplacement(context,
-              MaterialPageRoute(builder: (context) => ProfilePage()));
-              _animationController.reverse();
-            }
+              title: "Profile",
+              titleStyle: TextStyle(
+                  fontSize: 12
+              ),
+              icon: Icons.person,
+              iconColor: Colors.white,
+              bubbleColor: Colors.blue,
+              onPress: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => ProfilePage()));
+                _animationController.reverse();
+              }
           ),
           Bubble(
               title: "Payroll Logs",
@@ -100,7 +137,7 @@ class _UserDashboardState extends State<UserDashboard> with SingleTickerProvider
               bubbleColor: Colors.blue,
               onPress: () {
                 Navigator.pushReplacement(context,
-                MaterialPageRoute(builder: (context) => Payrolls()));
+                    MaterialPageRoute(builder: (context) => Payrolls()));
                 _animationController.reverse();
               }
           ),
